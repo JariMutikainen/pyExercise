0ca9d6de431eb980280453d835c6e479b2fb9ee2
Jari Mutikainen <jari.mutikainen@welho.net>
2018-10-29 10:27:58 +0200

    Started to work on the palindrome exercise.

---
+++ palindrome/palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(i_string):
+    """
+    def is_palindrome(i_string):
+
+    This function returns a True if the i_string is a palindrome.
+    Otherwise it returns a False. Determination is case insensitive and
+    the spaces in the i_string are ignored.
+    """
+    pass
+
+print(is_palindrome.__doc__)
