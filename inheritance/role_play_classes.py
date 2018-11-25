909b24372f10eb0f0eed31e7a72fd40ab2c9ec36
Jari Mutikainen <jari.mutikainen@welho.net>
2018-11-25 15:45:26 +0200

    Completed the inheritance exercise.

--- inheritance/role_play_classes.py
+++ inheritance/role_play_classes.py
@@ -9,14 +9,20 @@ class Character:
         self.level = level # The skill level of a role play character
 
     def __repr__(self):
-        s1 = f'{self.name} has {self.hp} hit points and a skill level' 
-        s2 = f' of {self.level}.'
+        # s1 = f'{self.name} has {self.hp} hit points and a skill level' 
+        # s2 = f' of {self.level}.'
+        s1 = '{} has {} hit points and a skill level'.format(self.name,self.hp)
+        s2 = ' of {}.'.format(self.level)
         return s1 + s2
 
 # Non-Player Character (= NPC) inherits everything from the base class 
 # 'Character'.
 class NPC(Character): 
-    pass
+    def __init__(self, name, hp, level):
+        super().__init__(name, hp, level)
+
+    def speak(self):
+        print("Hi, I'm just a humble non-player character.")
 
 
 
@@ -25,5 +31,8 @@ class NPC(Character):
 # Testing
 c1 = Character('Spider man', 100, 9)
 c2 = Character('Incredible Hulk', 200, 8)
+c3 = NPC('Hit man', 50, 7)
 print(c1)
 print(c2)
+print(c3)
+c3.speak()
