9057d4aae17aa69f796b85d0f3bca44480329ec2
Jari Mutikainen <jari.mutikainen@welho.net>
2018-12-03 13:12:04 +0200

    Completed the flip dictionary exercise.

--- exercises/more.py
+++ exercises/more.py
@@ -1,8 +1,9 @@
 """More comprehension exercises"""
 
 
-def flip_dict():
+def flip_dict(orig_dict):
     """Return a new dictionary that maps the original values to the keys."""
+    return {v: k for k, v in orig_dict.items()} 
 
 
 def get_ascii_codes():
@@ -19,3 +20,7 @@ def dict_from_tuple():
 
 def get_all_factors():
     """Return a dictionary mapping numbers to their factors."""
+
+# Testing
+# print(flip_dict(
+      # {'Python': "2015-09-15", 'Java': "2015-09-14", 'C': "2015-09-13"}))
