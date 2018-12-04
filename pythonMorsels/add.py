04ef414ec67bdacad3e9595f342e400b56938f29
Jari Mutikainen <jari.mutikainen@welho.net>
2018-12-04 10:10:02 +0200

    Completed the python morsels add exercise.

--- pythonMorsels/add.py
+++ pythonMorsels/add.py
@@ -14,19 +14,33 @@
 # [[2, -1, 3], [-3, 3, -3], [5, -6, 7]]
 
 
-def add(matrix1, matrix2):
+def add(*matricies):
     """
-    Adds two matricies to produce a sum matrix. 
+    Adds matricies to produce a sum matrix. 
     """
-    def add_two_rows(row1, row2):
+    def add_two_matricies(matrix1, matrix2):
         """
-        Adds two lists of numbers (= two rows of a two matricies) to produce
-        a sum_row like so: [1, 2, 3] + [10, 11, 12] = [11, 13, 15]
+        Adds two matricies to produce a sum matrix. 
         """
-        return [v1 + v2 for v1, v2 in zip(row1, row2)]
+        def add_two_rows(row1, row2):
+            """
+            Adds two lists of numbers (= two rows of a two matricies) to produce
+            a sum_row like so: [1, 2, 3] + [10, 11, 12] = [11, 13, 15]
+            """
+            if len(row1) != len(row2):
+                raise ValueError("Given matrices are not the same size.")
 
-    return [add_two_rows(r1, r2) for r1, r2 in zip(matrix1, matrix2)]
+            return [v1 + v2 for v1, v2 in zip(row1, row2)]
 
+        return [add_two_rows(r1, r2) for r1, r2 in zip(matrix1, matrix2)]
+
+    result = matricies[0]
+    for i in range(1,len(matricies)):
+        if len(matricies[0]) != len(matricies[i]):
+            raise ValueError("Given matrices are not the same size.")
+        result = add_two_matricies(result,matricies[i])
+
+    return result
 
 
 # Testing
@@ -37,5 +51,10 @@ def add(matrix1, matrix2):
 #matrix1 = [[1, -2, 3], [-4, 5, -6], [7, -8, 9]]
 #matrix2 = [[1, 1, 0], [1, -2, 3], [-2, 2, -2]]
 #print(add(matrix1, matrix2))
+
+#For the first bonus, modify your add function to accept and "add" any
+#number of lists-of-lists. 
+
+#print(add([[1, 9], [7, 3]], [[5, -4], [3, 3]], [[2, 3], [-3, 1]])) # [[8, 8], [7, 7]]
 
 
