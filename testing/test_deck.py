96062efbb4c03688d932bc9db4bbab0c715817b4
Jari Mutikainen <jari.mutikainen@welho.net>
2018-12-10 16:09:27 +0200

    Completed the unit testing exercise.

--- testing/test_deck.py
+++ testing/test_deck.py
@@ -17,7 +17,7 @@ class DeckTests(unittest.TestCase):
         self.assertEqual(str(c2), 'Q of Diamonds')
 
     def test_count(self):
-        """The count method works corrctly."""
+        """The count method works correctly."""
         self.assertEqual(self.d1.count(), 52, 'Full deck = 52 cards.')
         self.d1.deal_card()
         self.assertEqual(self.d1.count(), 51, '51 cards should be remaining.')
@@ -35,6 +35,23 @@ class DeckTests(unittest.TestCase):
         shuffled_deck = self.d1.shuffle()
         self.assertNotEqual(orig_deck, shuffled_deck,
             'The shuffled deck must be different from the non-shuffled deck.')
+        self.assertEqual(set(orig_deck), set(shuffled_deck),
+            'The shuffled and non-shuffled decks must contain the same cards.')
+        self.d1.deal_card()
+        with self.assertRaises(ValueError):
+            self.d1.shuffle()
+            #'Trying to shuffle an incomplete deck must raise an exception.'
+
+    def test_deal_hand(self):
+        """The deal_hand() method works correctly"""
+        self.d1.shuffle()
+        self.assertEqual(self.d1.count(), 52, 'A full deck must be 52 cards')
+        a_hand = self.d1.deal_hand(num_of_cards=5)
+        self.assertEqual(len(a_hand), 5, 'Must have dealt 5 cards.')
+        self.assertEqual(self.d1.count(), 47,
+                        '47 cards remaining in the deck.')
+        self.assertNotIn(a_hand, self.d1.cards,
+                        'The dealt cards must be missing from the deck.')
 
 if __name__ == '__main__':
     unittest.main()
