862355677f825752deb7970e42df99ec5bf33ebc
Jari Mutikainen <jari.mutikainen@welho.net>
2019-01-02 10:50:04 +0200

    Worked on the part 4 of the final exercises.

--- final/part3.py
+++ final/part3.py
@@ -1,48 +1,100 @@
-# This is the part 3 of the final exercises of the Python 3 boot camp.
-# 1. Write a function sum_up_diagonals(), which takes in a NxN matrix
-# of integers and calculates the sum of all the diagonal elements from
-# the upper left corner down to lower right corner AND from the lower left
-# corner up to the upper right corner.
-def sum_up_diagonals(matrix):
-    result = 0
-    for row in matrix:
-        for col row:
-            if row == col:
-                result += matrix[row][col]
-    return result
 
+# 6. Write a function two_list_dictionary(), which takes in two lists:
+# keys and values. Return a dictionary, which combines the two lists.
+# If there are too many keys insert null for their values. If there are
+# too many values, ignore the excess values.
+def two_list_dictionary(keys_, values_):
+    # Fill in None for the missing values.
+    while len(values_) < len(keys_):
+        values_ += [None]
+
+    return {k: v for k, v in zip(keys_, values_)}
+
+# Testing
+#print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3])) # {'a': 1, 'b': 2, 'c': 3, 'd': None}
+#print(two_list_dictionary(['a', 'b', 'c']  , [1, 2, 3, 4])) # {'a': 1, 'b': 2, 'c': 3}
+#print(two_list_dictionary(['x', 'y', 'z']  , [1,2])) # {'x': 1, 'y': 2, 'z': None}
+
+# 7. Write a function range_in_list(), which returns the sum of list elements
+# between the start and end indicies (inclusive). The default value for 
+# start is 0 and the default value of end is len(i_lst) - 1.
+# In case the value of end is out of range the sum must be calculated
+# all the way to the last element of the list.
+def range_in_list(lst, start=0,end=None):
+    if end == None:
+        end = len(lst)
+    return sum(lst[start:end+1])
+
+# Testing
+#print(range_in_list([1,2,3,4],0,2)) #  6
+#print(range_in_list([1,2,3,4],0,3)) # 10
+#print(range_in_list([1,2,3,4],1)) #  9
+#print(range_in_list([1,2,3,4])) # 10
+#print(range_in_list([1,2,3,4],0,100)) # 10
+#print(range_in_list([],0,1)) # 0
+
+# 8. Write a function same_frequency(), which takes in two integers. It returns
+# True, if both integers contain the same frequency of numbers.
+# See the examples below:
+# same_frequency(551122,221515) # True
+# same_frequency(321142,3212215) # False
+# same_frequency(1212, 2211) # True
+def same_frequency(num1, num2):
+    freqs1 = [0,0,0,0,0,0,0,0,0,0] 
+    freqs2 = [0,0,0,0,0,0,0,0,0,0] 
+    str1 = str(num1)
+    str2 = str(num2)
+    for ch in str1:
+        freqs1[int(ch)] += 1
+    for ch in str2:
+        freqs2[int(ch)] += 1
+    #return not cmp(freqs1, freqs2)
+    return all(val1 == val2 for val1, val2 in zip(freqs1, freqs2))
 
 
 # Testing
-list1 = [
-  [ 1, 2 ],
-  [ 3, 4 ]
-]
- 
-print(sum_up_diagonals(list1)) # 10
+#print(same_frequency(551122,221515)) # True
+#print(same_frequency(321142,3212215)) # False
+#print(same_frequency(1212, 2211)) # True
 
-#list2 = [
-#  [ 1, 2, 3 ],
-#  [ 4, 5, 6 ],
-#  [ 7, 8, 9 ]
-#]
-# 
-#print(sum_up_diagonals(list2)) # 30
-# 
-#list3 = [
-#  [ 4, 1, 0 ],
-#  [ -1, -1, 0],
-#  [ 0, 0, 9]
-#]
-#
-#print(sum_up_diagonals(list3)) # 11
-#
-#list4 = [
-#  [ 1, 2, 3, 4 ],
-#  [ 5, 6, 7, 8 ],
-#  [ 9, 10, 11, 12 ],
-#  [ 13, 14, 15, 16 ]
-#]
-# 
-#print(sum_up_diagonals(list4)) # 68
+# 9. Write a function nth(), which takes in a list and an index. It returns
+# the element at that index. If the index is negative it returns the nth
+# value starting from the end of the list.
+# nth(['a', 'b', 'c', 'd'], 1)  # 'b'
+# nth(['a', 'b', 'c', 'd'], -2) #  'c'
+# nth(['a', 'b', 'c', 'd'], 0)  # 'a'
+# nth(['a', 'b', 'c', 'd'], -4) #  'a'
+# nth(['a', 'b', 'c', 'd'], -1) #  'd'
+# nth(['a', 'b', 'c', 'd'], 3)  # 'd'
+def nth(lst, index):
+    return lst[index]
 
+# Testing
+#print(nth(['a', 'b', 'c', 'd'], 1))  # 'b'
+#print(nth(['a', 'b', 'c', 'd'], -2)) #  'c'
+#print(nth(['a', 'b', 'c', 'd'], 0))  # 'a'
+#print(nth(['a', 'b', 'c', 'd'], -4)) #  'a'
+#print(nth(['a', 'b', 'c', 'd'], -1)) #  'd'
+#print(nth(['a', 'b', 'c', 'd'], 3))  # 'd'
+
+# 10. Write a function find_the_duplicate(). It takes in a list of numbers
+# and returns the one, that appears in the list twice. In case there is no
+# such number there, the function returns None.
+# find_the_duplicate([1,2,1,4,3,12]) # 1
+# find_the_duplicate([6,1,9,5,3,4,9]) # 9
+# find_the_duplicate([2,1,3,4]) # None
+
+def find_the_duplicate(lst):
+    seen_before = []
+    for n in lst:
+        if n in seen_before:
+            return n
+        else:
+            seen_before += [n]
+    return None
+
+
+# Testing
+#print(find_the_duplicate([1,2,1,4,3,12])) # 1
+#print(find_the_duplicate([6,1,9,5,3,4,9])) # 9
+#print(find_the_duplicate([2,1,3,4])) # None
---
+++ final/part4.py
@@ -0,0 +1,115 @@
+# This is the part 3 of the final exercises of the Python 3 boot camp.
+# 1. Write a function sum_up_diagonals(), which takes in a NxN matrix
+# of integers and calculates the sum of all the diagonal elements from
+# the upper left corner down to lower right corner AND from the lower left
+# corner up to the upper right corner.
+
+# NOTICE, THIS SOLUTION COUNTS THE MIDDLE ELEMENT OF AN ODD SIZED MATRIX
+# TWICE, WHICH IS INCORRECT (IMHO). HOWEVER, THE UNIT TESTS OF THE EXERCISE
+# SEEM TO EXPECT IT TO BEHAVE THAT WAY. SO I DON'T CARE EITHER.
+
+def sum_up_diagonals(matrix):
+    def pick_elements(matrx):
+        '''Returns the list of the diagonal elements of matrx
+           from the upper left corner down to the lower right corner
+        '''
+        diag_list = []
+        for r, row in enumerate(matrx):
+            print(r, row)
+            for c, elem in enumerate(row):
+                if c == r:
+                    diag_list += [elem]
+        return diag_list
+
+    def reverse_matrix(matrx):
+        '''Reverses the order of the rows in the matrx.'''
+        return matrx[::-1]
+
+    elements = []
+    elements += pick_elements(matrix)
+    elements += pick_elements(reverse_matrix(matrix))
+
+    return sum(elements)
+
+
+
+
+# Testing
+#list1 = [
+#  [ 1, 2 ],
+#  [ 3, 4 ]
+#]
+# 
+#print(sum_up_diagonals(list1)) # 10
+#
+#list2 = [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+#]
+# 
+#print(sum_up_diagonals(list2)) # 30
+# 
+#list3 = [
+#  [ 4, 1, 0 ],
+#  [ -1, -1, 0],
+#  [ 0, 0, 9]
+#]
+#
+#print(sum_up_diagonals(list3)) # 11
+#
+#list4 = [
+#  [ 1, 2, 3, 4 ],
+#  [ 5, 6, 7, 8 ],
+#  [ 9, 10, 11, 12 ],
+#  [ 13, 14, 15, 16 ]
+#]
+# 
+#print(sum_up_diagonals(list4)) # 68
+
+# 2. Write a function min_max_key_in_dictionary(dict_), which returns a list
+# of two integers: the smallest key and the largest key of that dictionary
+# min_max_key_in_dictionary({2:'a', 7:'b', 1:'c',10:'d',4:'e'}) # [1,10]
+# min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"}) # [1,4]
+def min_max_key_in_dictionary(dict_):
+    return [min(dict_.keys()), max(dict_.keys()) ]
+
+
+# Testing
+#print(min_max_key_in_dictionary({2:'a', 7:'b', 1:'c',10:'d',4:'e'})) # [1,10]
+#print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"})) # [1,4]
+
+# 3. Write a function find_greater_numbers(list_), which returns the number
+# of times an integer is followed by some greater integer. Like so:
+# find_greater_numbers([6,1,2,7]) -> 4 .... because ...
+# The (weird) logic of this exercise is as follows:
+# 1. 2>1
+# 2. 7>6
+# 3. 7>1
+# 4. 7>2
+def find_greater_numbers(ints):
+    cnt = 0
+    for i, val in enumerate(ints):
+        #print(f'{i}: {val}, {ints[:i]}')
+        # Compare val to all the values sitting before it in the list
+        for n in ints[:i]:
+            if val > n:
+                cnt += 1
+    return cnt
+
+# Testing
+#print(find_greater_numbers([1,2,3])) # 3
+#print(find_greater_numbers([6,1,2,7])) # 4
+#print(find_greater_numbers([5,4,3,2,1])) # 0
+#print(find_greater_numbers([])) # 0
+
+# 4. Write a function two_oldest_ages(ages), which returns the two largest
+# integers in the list in the ascening order.
+def two_oldest_ages(ages):
+    ages.sort()
+    return ages[-2:]
+
+# Testing
+#print(two_oldest_ages( [1, 2, 10, 8] )) # [8, 10]
+#print(two_oldest_ages( [6,1,9,10,4] )) # [9,10]
+#print(two_oldest_ages( [4,25,3,20,19,5] )) # [20,25]
