1b48b3963d991dd3260770fb0e87e386e42303b5
Jari Mutikainen <jari.mutikainen@welho.net>
2019-01-12 10:27:03 +0200

    Continued with the substitution exercises.

--- pyCon2017/substitution.py
+++ pyCon2017/substitution.py
@@ -17,16 +17,26 @@ def normalize_jpeg(filename):
 
 def normalize_whitespace(string):
     """Replace all runs of whitespace with a single space."""
-    return re.sub(r'(\s+)', ' ', string)
+    return re.sub(r'\s+', ' ', string)
 
 
 def compress_blank_lines(string, max_blanks):
     """Compress N or more empty lines into just N empty lines."""
 
+#print(compress_blank_lines("a\n\n\n\nb\n\n\nc", max_blanks=2))
+
 
 def normalize_domain(string):
     """Normalize all instances of treyhunner.com URLs."""
+    exp = re.compile(r'(http://treyhunner.com)|(http://www.treyhunner.com)')
+    return re.sub(exp, 'https://treyhunner.com', string)
 
 
 def convert_linebreaks(string):
     """Convert linebreaks to HTML."""
+    paragraphs = re.split(r'\n{2,}', string)
+    o_string = ''
+    for text in paragraphs:
+        o_string = o_string + '<p>' + re.sub(r'\n', '<br>', text) + '</p>'
+    return o_string
+
