53539ec9ab0880b3af24fe3482cf4bdff52b8961
Jari Mutikainen <jari.mutikainen@welho.net>
2019-01-14 10:23:38 +0200

    Struggled with the advanced exercises.

--- pyCon2017/lookahead.py
+++ pyCon2017/lookahead.py
@@ -12,17 +12,39 @@ with open('dictionary.txt') as dict_file:
 
 def have_all_vowels(dictionary=dictionary):
     """Return all words at most 9 letters long that contain all vowels."""
-    rex = re.compile(r'\b[aeiouAEIOU]+\b') 
-    return [word for word in re.split(r'\W+', dictionary)
-            if len(word) < 10 and bool(re.search(rex, word))]
-
+    rex = re.compile(r'\b(?=\w*a)(?=\w*e)(?=\w*i)(?=\w*o)(?=\w*u)(\w{1,9})\b')
+    return re.findall(rex, dictionary)
 
 def no_repeats(dictionary=dictionary):
     """Return all words with 10 or more letters and no repeating letters."""
+    def all_letters_are_unique(word):
+        '''Returns True if all the letters of the word are unique.'''
+        seen_before = ''
+        for ch in word:
+            if ch in seen_before:
+                return False
+            else:
+                seen_before += ch
+        return True
+
+    return [word for word in re.split(r'\W+', dictionary)
+            if len(word) > 9 and all_letters_are_unique(word)]
 
 
 def to_pig_latin(phrase):
     """Convert English phrase to Pig Latin."""
+    rex = re.compile(r'\b([^aeiou]+u?)(\w+)\b')
+    pig_words = []
+    for word in re.split(r'\W+', phrase):
+        if word[0] in 'aeiou':
+            pig_words.append(word + 'ay')
+        else:
+            mo = rex.search(word)
+            pig_words.append(mo.group(2) + mo.group(1) + 'ay')
+        #print(pig_words)
+    return ' '.join(pig_words)
+
+print(to_pig_latin('pig squeack trust quack enqueue'))
 
 
 def encode_ampersands(phrase):
