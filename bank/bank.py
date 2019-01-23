64f20a93df518694964f5d79173c1be4c8c66249
Jari Mutikainen <jari.mutikainen@welho.net>
2019-01-23 16:21:17 +0200

    Completed the banking exercise.

--- bank/bank.py
+++ bank/bank.py
@@ -19,6 +19,12 @@ class Bank:
     def __init__(self):
         self.accounts = {}
 
+    @staticmethod
+    def get_details():
+        owner = input('Enter your name: ')
+        acc_num = input('Enter your 5-digit account number: ')
+        return (owner, acc_num)
+
     def creation(self):
         owner = input('Enter your name: ')
         initial_depo = input('Enter your initial deposit amount: ')
@@ -27,31 +33,36 @@ class Bank:
             # Keep generating 5-digit random numbers until you find an
             # unused one.
             account_number = randint(1,100000)
-        self.accounts[account_number] = Account(owner, initial_depo)
-        print(f'\nHello {owner}\n'
-              f'The account number of your new savings account is '
-              f'{account_number:0>5d}\n'
-              f'The initial balance of that account was set to {initial_depo}')
+        acc_num = f'{account_number:0>5d}'
+        self.accounts[acc_num] = Account(owner, acc_num, initial_depo)
 
-    def deposition(self):
-        print('deposition')
+    def deposit(self):
+        owner, acc_num = self.get_details()
+        depo_amount = input('Enter amount to be deposited: ')
+        self.accounts[acc_num].deposit(owner, acc_num, depo_amount)
 
-    def withdrawal(self):
-        print('withdrawal')
+    def withdraw(self):
+        owner, acc_num = self.get_details()
+        wd_amount = input('Enter amount to be withdrawn: ')
+        self.accounts[acc_num].withdraw(owner, acc_num, wd_amount)
 
     def query(self):
-        print('query')
+        owner, acc_num = self.get_details()
+        try:
+            self.accounts[acc_num].show_balance(owner, acc_num)
+        except KeyError:
+            print(f'\n\tAccount number {acc_num} does not exist.')
 
-    def exition(self):
-        print('Thank you for using our banking system.')
+    def exit_(self):
+        print('\n\tThank you for using our banking system.')
         exit(0)
 
     def run_bank(self):
         actions = {'1': self.creation,
-                   '2': self.deposition,
-                   '3': self.withdrawal,
+                   '2': self.deposit,
+                   '3': self.withdraw,
                    '4': self.query,
-                   '5': self.exition}
+                   '5': self.exit_}
 
         while True:
             print('\nCreate new savings account:          1\n'
@@ -64,9 +75,6 @@ class Bank:
                 actions[choice]()
             except KeyError:
                 print('\nIllegal input. try again.')
-
-
-
 
 
 if __name__ == '__main__':
